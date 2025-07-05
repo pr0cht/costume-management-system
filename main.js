0eff1f875ebe50be6187a6ee2a668b2ff6ea7ae2
pr0cht <procht01@gmail.com>
2025-07-05 11:34:39 +0800

    init

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, Tray } = require('electron');
+
+app.whenReady().then(() => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    app.quit();
+  });
+    const tray = new Tray('assets/tray_icon.jpg');
+    const contextMenu = require('electron').Menu.buildFromTemplate([
+    {
+        label: 'Show App',
+        click: () => {
+        mainWindow.show();
+        }
+    },
+    {
+        label: 'Quit',
+        click: () => {
+        app.quit();
+        }
+    }
+    ]);
+
+    tray.setToolTip('Costume Management System');
+    tray.setContextMenu(contextMenu);
+});
+
+
